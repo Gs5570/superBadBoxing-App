798110769a302914538f9591b668fc3f0229f500
Galekwan Sango <galekwansango@gmail.com>
2026-06-12 14:54:26 -0400

    defined anchor tag absolute path route to fix angular appending child route to current route when anchor elt is clicked

--- src/app/components/pages/about/about.ts
+++ src/app/components/pages/about/about.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Footer } from '../../footer/footer';
 import { Header } from '../../header/header';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [Footer, Header],
+  imports: [Footer, Header, RouterLink, RouterOutlet],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
